b794c46ea0678d43e813afdca989b6c9ab99557d
Lorenzo Maiorfi <maiorfi@gmail.com>
2018-04-25 09:01:49 +0200

    Commit iniziale

---
+++ SimpleOrleansSample/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Orleans2GettingStarted
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var siloBuilder = new SiloHostBuilder()
+                .UseLocalhostClustering()
+                .UseDashboard(options => { })
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "Orleans2GettingStarted";
+                })
+                .Configure<EndpointOptions>(options =>
+                    options.AdvertisedIPAddress = IPAddress.Loopback)
+                .ConfigureLogging(logging => logging.AddConsole());
+
+            using (var host = siloBuilder.Build())
+            {
+                await host.StartAsync();
+
+                var clientBuilder = new ClientBuilder()
+                    .UseLocalhostClustering()
+                    .Configure<ClusterOptions>(options =>
+                    {
+                        options.ClusterId = "dev";
+                        options.ServiceId = "Orleans2GettingStarted";
+                    })
+                    .ConfigureLogging(logging => logging.AddConsole());
+
+                using (var client = clientBuilder.Build())
+                {
+                    await client.Connect();
+
+                    var random = new Random();
+                    
+                    while (true)
+                    {
+                        int grainId = random.Next(0, 500);
+                        double temperature = random.NextDouble() * 40;
+                        var sensor = client.GetGrain<ITemperatureSensorGrain>(grainId);
+                        await sensor.SubmitTemperatureAsync((float)temperature);
+                    }
+                }
+            }
+        }
+    }
+}
